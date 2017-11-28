acc5cac4f1dba0318adce5550d22f69f02940715
lpetcana-gpsw <lpetcana@gopro.com>
2017-11-28 18:11:24 +0200

    a little refactoring

--- handlers.go
+++ handlers.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/gorilla/mux"
+	"net/http"
+	"html"
+	"encoding/json"
+)
+
+func Index(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Println("hello")
+	log.Println("Index called")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars:=mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo Show:", todoId)
+}
+
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Not Implemented"))
+})
+
