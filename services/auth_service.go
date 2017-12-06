4a2fd889ca25a6152f10e990759b6ba1b0034e89
lpetcana-gpsw <lpetcana@gopro.com>
2017-12-06 20:39:23 +0200

    added auth controller/service

---
+++ services/auth_service.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"smart-home-automation/models"
+	"net/http"
+	"encoding/json"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func Login(requestUser *models.User) (int, []byte) {
+	authBackend := authentication.InitJWTAuthenticationBackend()
+
+	if authBackend.Authenticate(requestUser) {
+		token, err := authBackend.GenerateToken(requestUser.UUID)
+		if err != nil {
+			return http.StatusInternalServerError, []byte("")
+		} else {
+			response, _ := json.Marshal(parameters.TokenAuthentication{token})
+			return http.StatusOK, response
+		}
+	}
+
+	return http.StatusUnauthorized, []byte("")
+}
+
+func Logout(req *http.Request) error {
+	authBackend := authentication.InitJWTAuthenticationBackend()
+	tokenRequest, err := jwt.ParseFromRequest(req, func(token *jwt.Token) (interface{}, error) {
+		return authBackend.PublicKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	tokenString := req.Header.Get("Authorization")
+	return authBackend.Logout(tokenString, tokenRequest)
+}
