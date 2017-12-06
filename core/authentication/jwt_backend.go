0839c5e5e98e898927b73cc073ab72388f727b69
lpetcana-gpsw <lpetcana@gopro.com>
2017-12-06 22:26:19 +0200

    added generate token method

--- core/authentication/jwt_backend.go
+++ core/authentication/jwt_backend.go
@@ -7,6 +7,12 @@ import (
 	"bufio"
 	"encoding/pem"
 	"crypto/x509"
+	"smart-home-automation/core/redis"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+	"golang.org/x/crypto/bcrypt"
+	"smart-home-automation/models"
+	"github.com/pborman/uuid"
 )
 
 type JWTAuthenticationBackend struct {
@@ -93,4 +99,46 @@ func getPrivateKey() *rsa.PrivateKey {
 func (backend *JWTAuthenticationBackend) Logout(tokenString string, token *jwt.Token) error {
 	redisConn := redis.Connect()
 	return redisConn.SetValue(tokenString, tokenString, backend.getTokenRemainingValidity(token.Claims.(jwt.MapClaims)["exp"]))
+}
+
+func (backend *JWTAuthenticationBackend) getTokenRemainingValidity(timestamp interface{}) int {
+	if validity, ok := timestamp.(float64); ok {
+		tm := time.Unix(int64(validity), 0)
+		remainer := tm.Sub(time.Now())
+		if remainer > 0 {
+			return int(remainer.Seconds() + expireOffset)
+		}
+	}
+	return expireOffset
+}
+
+func (backend *JWTAuthenticationBackend) Authenticate(user *models.User) bool {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("testing"), 10)
+
+	testUser := models.User{
+		UUID:     uuid.New(),
+		Username: "haku",
+		Password: string(hashedPassword),
+	}
+
+	return user.Username == testUser.Username && bcrypt.CompareHashAndPassword([]byte(testUser.Password), []byte(user.Password)) == nil
+}
+
+func (backend *JWTAuthenticationBackend) GenerateToken(userUUID string) (string, error) {
+	token :=jwt.New(jwt.SigningMethodRS512)
+
+	token.Claims = jwt.MapClaims{
+		"exp": time.Now().Add(time.Hour * time.Duration(settings.Get().JWTExpirationDelta)).Unix(),
+		"iat": time.Now().Unix(),
+		"sub": userUUID,
+	}
+
+	tokenString,err:=token.SignedString(backend.privateKey)
+
+	if err!=nil {
+		panic(err)
+		return "", err
+	}
+
+	return tokenString, nil
 }
