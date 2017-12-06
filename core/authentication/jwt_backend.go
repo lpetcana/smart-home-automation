9181f97a4d0b74de8714605f6cfff29d496bae6f
lpetcana-gpsw <lpetcana@gopro.com>
2017-12-06 21:32:48 +0200

    created methods for reading public and private keys

--- core/authentication/jwt_backend.go
+++ core/authentication/jwt_backend.go
@@ -3,6 +3,10 @@ package authentication
 import (
 	"crypto/rsa"
 	"os"
+	"smart-home-automation/settings"
+	"bufio"
+	"encoding/pem"
+	"crypto/x509"
 )
 
 type JWTAuthenticationBackend struct {
@@ -35,6 +39,53 @@ func getPublicKey() *rsa.PublicKey {
 		panic(err)
 	}
 
+	pemfileinfo, _ := publicKeyFile.Stat()
+	var size int64 = pemfileinfo.Size()
+	pembytes := make([]byte, size)
 
+	buffer := bufio.NewReader(publicKeyFile)
+	_, err = buffer.Read(pembytes)
+	data, _ := pem.Decode([]byte(pembytes))
 
+	publicKeyFile.Close()
+
+	publicKeyImported, err := x509.ParsePKIXPublicKey(data.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	rsaPub, ok := publicKeyImported.(*rsa.PublicKey)
+
+	if !ok {
+		panic(err)
+	}
+
+	return rsaPub
+
+}
+
+func getPrivateKey() *rsa.PrivateKey {
+	privateKeyFile, err := os.Open(settings.Get().PrivateKeyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	pemfileinfo, _ := privateKeyFile.Stat()
+	var size int64 = pemfileinfo.Size()
+	pembytes := make([]byte, size)
+
+	buffer := bufio.NewReader(privateKeyFile)
+	_, err = buffer.Read(pembytes)
+
+	data, _ := pem.Decode([]byte(pembytes))
+
+	privateKeyFile.Close()
+
+	privateKeyImported, err := x509.ParsePKCS1PrivateKey(data.Bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return privateKeyImported
 }
